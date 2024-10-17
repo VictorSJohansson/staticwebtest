3384a782780b8f417d4504b05f447f4bbcc8f2dd
VictorSJohansson <victor.s.johansson@gmail.com>
2024-10-17 16:48:04 +0200

    added json

--- api/function_app.py
+++ api/function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -17,9 +18,14 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully FROM THE API.")
+        return func.HttpResponse(
+            json.dumps({ "message": f"Hello, {name}. This HTTP triggered function executed successfully." }),
+            mimetype="application/json",
+            status_code=200
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            json.dumps({ "message": "Name not provided" }),
+            mimetype="application/json",
+            status_code=200
         )
